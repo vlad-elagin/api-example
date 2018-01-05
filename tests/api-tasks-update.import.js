acbf1e1de2d125c4519b7bcf6cd9929704fd1e66
Vlad Elagin <vlad.elagin@keenethics.com>
2018-01-05 18:15:53 +0200

    another update task WIP commit

--- tests/api-tasks-update.import.js
+++ tests/api-tasks-update.import.js
@@ -8,6 +8,14 @@ const updateTaskApiTest = () => {
     ({ token } = await prepareUser());
     done();
   });
+  // prepare fresh task before each test
+  let task;
+  beforeEach(async (done) => {
+
+  });
+
+  // look at that retarded idiot who's using POST and stupidly grinning when tests are passed
+  // dont forget to pass tasks id and write test for case when id is invalid
 
   it('Should fail if no data sent.', async () => {
     expect.assertions(2);
@@ -54,6 +62,23 @@ const updateTaskApiTest = () => {
       expect(res.text).toBe('Data is invalid, check your validation services.');
     });
     done();
+  });
+
+  it('Should fail if assignee not found', async (done) => {
+    const res = await request(app)
+      .patch('/api/tasks/')
+      .send({
+        assignee: 'me',
+      })
+      .set('Authorization', `Bearer ${token}`);
+    expect(res.statusCode).toBe(400);
+    expect(res.text).toBe('Executor not found.');
+    done();
+  });
+
+  // user can update only heading, description, priority, personality, author and completed fields
+  it('Shouldn\'t update protected task fields', async () => {
+
   });
 };
 
